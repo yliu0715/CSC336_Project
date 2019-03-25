9b0914d405ad022906575fdab38754dfb1d141df
yliu0715 <yliu5683@gmail.com>
2019-03-24 23:30:10 -0400

    created loginform for loginpage

--- frontend/src/components/SignIn/SignIn.js
+++ frontend/src/components/SignIn/SignIn.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SignIn = () => {
-  return (
-    <div>
-      This is the SignIn page
-    </div>
-  )
+import './SignIn.css';
+
+class SignIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: '',
+      password: ''
+    }
+  }
+
+  onUsernameChange = (event) => {console.log(event.target.value)}
+  onPasswordChange = (event) => {console.log(event.target.value)}
+
+  render() {
+    return (
+      <div className="container">
+        <div className="form-bg">
+          <form>
+            <h2 className="text-center">Login Form</h2>
+            <div className="form-group">
+              <label>Username</label>
+              <input type="email" className="form-control" placeholder="Enter Username"/>
+            </div>
+            <div className="form-group">
+              <label>Password</label>
+              <input type="password" className="form-control" placeholder="Enter Password"/>
+            </div>
+            <div>
+              <a href="#"> Don't Have An Account? Register</a>
+            </div>
+            <div className="text-center mt-3">
+              <button type="submit" className="btn btn-primary btn-block">Login</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default SignIn;
