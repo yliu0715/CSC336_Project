6ccf8ade98634b43373b54f856c8946fbd1d63fb
yliu0715 <yliu5683@gmail.com>
2019-03-29 21:43:24 -0400

    updated class to className

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -3,29 +3,29 @@ import React from 'react';
 const Navbar = () => {
   return (
     <div>
-        <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
-		  <ul class="navbar-nav mr-auto">
-			<li class="nav-item">
-			  <a class="nav-link" href="#">Home</a>
-			</li>
-			<li class="nav-item">
-			  <a class="nav-link" href="#">About</a>
-			</li>
-			<li class="nav-item">
-			  <a class="nav-link" href="#">FAQ</a>
-			</li>
-		  </ul>
-		  <ul class="navbar-nav">
-			<li class="nav-item dropdown">
-			  <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">My Account</a>
-			  <div class="dropdown-menu">
-				<a class="dropdown-item" href="#">My Profile</a>
-				<a class="dropdown-item" href="#">My Rooms</a>
-				<a class="dropdown-item" href="#">Logout</a>
-			  </div>
-			</li>
-		  </ul>
-		</nav>
+      <nav className="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+  		  <ul className="navbar-nav mr-auto">
+  			<li className="nav-item">
+  			  <a className="nav-link" href="#">Home</a>
+  			</li>
+  			<li className="nav-item">
+  			  <a className="nav-link" href="#">About</a>
+  			</li>
+  			<li className="nav-item">
+  			  <a className="nav-link" href="#">FAQ</a>
+  			</li>
+  		  </ul>
+  		  <ul className="navbar-nav">
+    			<li className="nav-item dropdown">
+    			  <a className="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">My Account</a>
+    			  <div className="dropdown-menu">
+      				<a className="dropdown-item" href="#">My Profile</a>
+      				<a className="dropdown-item" href="#">My Rooms</a>
+      				<a className="dropdown-item" href="#">Logout</a>
+    			  </div>
+    			</li>
+  		  </ul>
+		  </nav>
     </div>
   )
 }
