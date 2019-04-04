eaa644f5d02fbc07dfa0fd4912c58494d96ac3b7
Oscar <oscarchong11@gmail.com>
2019-04-03 23:20:54 -0400

    updated Homepage

--- frontend/src/components/Homepage/Homepage.js
+++ frontend/src/components/Homepage/Homepage.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import Navbar from '../Navbar/Navbar';
+import './Homepage.css'
+
 
 const Homepage = () => {
+
   return (
-    <div>
-      This is the homepage
+    <div className="text-center">
+      <Navbar/>
+      <br/>
+      <br/>
+      <br/>
+      <button className="roomButton">Room 1</button>
+      <br/>
+      <br/>
+      <button className="roomButton">Room 2</button>
+      <br/>
+      <br/>
+      <button className="roomButton">Room 3</button>
+      <br/>
+      <br/>
+      <button className="roomButton">Room 4</button>
+
     </div>
+    
   )
 }
+
 
 export default Homepage;
