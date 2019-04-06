34f0cba89ee53ec4536bb37971f7df83cd34fb93
Oscar <oscarchong11@gmail.com>
2019-04-06 17:27:13 -0400

    added borders to Rooms

--- frontend/src/components/Room/Room.js
+++ frontend/src/components/Room/Room.js
@@ -4,14 +4,10 @@ import './Room.css';
 
 const Room = (props) => {
   return (
-    <div className="text-center">
-      <button className="roomButton">
-        <div>
+    <div className="text-center roomButton">
           <h2>{`Room Name: ${props.title}`}</h2>
           <h3>{`Creator: ${props.id}`}</h3>
           <h4>{`Description: ${props.body}`}</h4>
-        </div>
-      </button>
     </div>
   )
 }
