8566beabd8750b2a2c8c9b7a8dcc08af03dbfff3
yliu0715 <yliu5683@gmail.com>
2019-04-06 23:36:22 -0400

    moved routes to app and updated navbar to render based on authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import HomePage from './components/Homepage/Homepage';
 import Navbar from './components/Navbar/Navbar';
-import Homepage from './components/Homepage/Homepage';
+import Register from './components/Register/Register';
+import Roompage from './components/Roompage/Roompage';
+import SignIn from './components/SignIn/SignIn';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = { users : [] }
+    this.state = {
+      users : [],
+      authenticated : true
+    }
   }
+
+  onLogOut = () => this.setState( { authenticated : false } );
 
   componentDidMount() {
     //This acts as a fake backend, we will replace this with fetching from our db later
@@ -19,10 +28,15 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Homepage users={this.state.users} />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar authenticated={ this.state.authenticated } onLogOut={ this.onLogOut } />
+          <Route exact path="/" render={(props) => <HomePage users={ this.state.users } /> } />
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={SignIn} />
+          <Route path="/roompage" component={Roompage} />
+        </div>
+      </Router>
     );
   }
 }
